3e90b4283fc1e53fec4e16b1266039a134b82664
Jose Luis Gomez <montalgex18@gmail.com>
2024-12-02 22:25:17 -0600

    documentoPDF

--- src/middleware/upLoadFile.js
+++ src/middleware/upLoadFile.js
@@ -1,7 +1,14 @@
 import multer from "multer";
 
 // Configuración para guardar el archivo en memoria
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+const upload = multer({ storage: storage });
 
 export default upload;
