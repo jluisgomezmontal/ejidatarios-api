16e5bc503937e1e54e566f0ffce042f1453b4627
Jose Luis Gomez <montalgex18@gmail.com>
2025-01-06 13:43:38 -0600

    find 1

--- src/controllers/terrenoController.js
+++ src/controllers/terrenoController.js
@@ -1,4 +1,3 @@
-import Ejidatario from "../models/Ejidatario.js";
 import Terreno from "../models/Terreno.js";
 
 export const createTerreno = async (req, res) => {
@@ -16,21 +15,25 @@ export const createTerreno = async (req, res) => {
 };
 
 export const getTerrenos = async (req, res) => {
-    try {
-        const terrenos = await Terreno.find().populate('propietario').populate('propietarioOrigen');
-        res.status(200).json(terrenos);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  try {
+    const terrenos = await Terreno.find()
+      .populate("propietario")
+      .populate("propietarioOrigen");
+    res.status(200).json(terrenos);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 export const getTerrenosById = async (req, res) => {
-    try {
-        const terrenos = await Terreno.find({_id : req.params.id}).populate('propietario').populate('propietarioOrigen');
-        res.status(200).json(terrenos);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  try {
+    const terrenos = await Terreno.find({ _id: req.params.id })
+      .populate("propietario")
+      .populate("propietarioOrigen");
+    res.status(200).json(terrenos);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 export const updateTerreno = async (req, res) => {
@@ -45,30 +48,52 @@ export const updateTerreno = async (req, res) => {
 };
 
 export const getTerrenosBySujeto = async (req, res) => {
-    try {
-        const terrenos = await Terreno.find({iD_Ejidatario : req.params.idSujeto}).populate('propietario').populate('propietarioOrigen');
-        res.status(200).json(terrenos);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  try {
+    const terrenos = await Terreno.find({ iD_Ejidatario: req.params.idSujeto })
+      .populate("propietario")
+      .populate("propietarioOrigen");
+    res.status(200).json(terrenos);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 export const getNumeroCertificado = async (req, res) => {
-    try {
-        const terrenos = await Terreno.findOne({numeroCertificado : req.params.numeroCertificado}).populate('propietario').populate('propietarioOrigen');
-        res.status(200).json(terrenos);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  try {
+    const terrenos = await Terreno.findOne({
+      numeroCertificado: req.params.numeroCertificado,
+    })
+      .populate("propietario")
+      .populate("propietarioOrigen");
+    res.status(200).json(terrenos);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 export const getNumeroParcela = async (req, res) => {
-    try {
-        const terrenos = await Terreno.findOne({numeroParcela : req.params.numeroParcela}).populate('propietario').populate('propietarioOrigen');
-        res.status(200).json(terrenos);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  try {
+    const terrenos = await Terreno.findOne({
+      numeroParcela: req.params.numeroParcela,
+    })
+      .populate("propietario")
+      .populate("propietarioOrigen");
+    res.status(200).json(terrenos);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+export const getPosesionario = async (req, res) => {
+  try {
+    const terrenos = await Terreno.find({
+      numeroParcela: req.params.numeroParcela,
+    })
+      .populate("propietario")
+      .populate("propietarioOrigen");
+    res.status(200).json(terrenos);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 export const getFileTerreno = async (req, res) => {
--- src/routes/terrenos.js
+++ src/routes/terrenos.js
@@ -1,24 +1,26 @@
 import express from "express";
 import {
-    createTerreno, getFileTerreno,
-    getNumeroCertificado, getNumeroParcela,
-    getTerrenos,
-    getTerrenosById,
-    getTerrenosBySujeto
+  createTerreno,
+  getFileTerreno,
+  getNumeroCertificado,
+  getNumeroParcela,
+  getPosesionario,
+  getTerrenos,
+  getTerrenosById,
+  getTerrenosBySujeto,
 } from "../controllers/terrenoController.js";
 import upload from "../middleware/upLoadFile.js";
 
 const router = express.Router();
 
 // Crear un nuevo terreno
-router.post("/",upload.single("documentoPDF") , createTerreno);
+router.post("/", upload.single("documentoPDF"), createTerreno);
 router.get("/", getTerrenos);
 router.get("/:id", getTerrenosById);
 router.get("/sujeto/:idSujeto", getTerrenosBySujeto);
 router.get("/certificado/:numeroCertificado", getNumeroCertificado);
 router.get("/parcela/:numeroParcela", getNumeroParcela);
+router.get("/origen/:numeroParcela", getPosesionario);
 router.get("/files/:fileName", getFileTerreno);
-
-
 
 export default router;
