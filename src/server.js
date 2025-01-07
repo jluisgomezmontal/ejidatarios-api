13392a3f0bbcc3956eac5eac879cd13ab99382c7
Jose Luis Gomez <montalgex18@gmail.com>
2025-01-07 17:06:55 -0600

    path

--- src/server.js
+++ src/server.js
@@ -11,23 +11,26 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json()); // Reemplaza bodyParser.json()
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://ejido-san-marcos.netlify.app']
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://ejido-san-marcos.netlify.app"],
+  })
+);
 
 // Conexión a la base de datos
 mongoose
-    .connect(
-        "mongodb+srv://luis:220690@ejido.lpplq.mongodb.net/?retryWrites=true&w=majority&appName=ejido"
-    )
-    .then(() => console.log("Conectado a MongoDB"))
-    .catch((err) => console.error("Error al conectar a MongoDB:", err));
+  .connect(
+    "mongodb+srv://luis:220690@ejido.lpplq.mongodb.net/?retryWrites=true&w=majority&appName=ejido"
+  )
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((err) => console.error("Error al conectar a MongoDB:", err));
 
 // Rutas
 app.use("/api/ejidatarios", ejidatarioRoutes);
 app.use("/api/terrenos", terrenos);
 app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/auth", authRoute); // Rutas de autenticación
+app.use("/uploads", express.static(path.join("/var/data/uploads/ejidatarios")));
 
 // Iniciar servidor
 app.listen(PORT, () => {
